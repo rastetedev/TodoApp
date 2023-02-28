a5a3f686c5aba1eb683d318a4b21d0bc3352a23f
Raul Astete <rastete@acculynx.com>
2023-02-28 13:51:44 -0500

    Setup Menu Note List

--- app/src/main/java/com/rastete/todoapp/presentation/features/note_list/NoteListFragment.kt
+++ app/src/main/java/com/rastete/todoapp/presentation/features/note_list/NoteListFragment.kt
@@ -1,10 +1,11 @@
 package com.rastete.todoapp.presentation.features.note_list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.rastete.todoapp.R
 import com.rastete.todoapp.databinding.FragmentNoteListBinding
@@ -13,6 +14,9 @@ class NoteListFragment : Fragment() {
 
     private var _binding: FragmentNoteListBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var actionSortHighPriorityItem: MenuItem
+    private lateinit var actionSortLowPriorityItem: MenuItem
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,9 +30,46 @@ class NoteListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val menuHost: MenuHost = requireActivity()
+        setupMenu(menuHost)
+
         binding.fabAddNoteNoteListF.setOnClickListener {
             findNavController().navigate(R.id.action_NoteListFragment_to_AddNoteFragment)
         }
+    }
+
+    private fun setupMenu(menuHost: MenuHost) {
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_note_list, menu)
+                actionSortHighPriorityItem = menu.findItem(R.id.action_sort_high_priority)
+                actionSortLowPriorityItem = menu.findItem(R.id.action_sort_low_priority)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.action_delete_all -> {
+                        true
+                    }
+                    R.id.action_sort_high_priority -> {
+                        menuItem.isChecked = true
+                        actionSortLowPriorityItem.isChecked = false
+                        true
+                    }
+                    R.id.action_sort_low_priority -> {
+                        menuItem.isChecked = true
+                        actionSortHighPriorityItem.isChecked = false
+
+                        true
+                    }
+                    else -> {
+                        true
+                    }
+
+                }
+            }
+
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
     override fun onDestroyView() {
